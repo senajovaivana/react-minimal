34fa911f3ce37e70897d5873abbfe669eb7002f8
senajovaivana <senajovaivana@gmail.com>
2019-10-15 12:28:27 +0200

    Update README.md

--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const App = () => (
-  <h1>Minimal React</h1>  
-)
-
-export default App
---
+++ src/MyApp.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const MyApp = () => (
+  <h1>Minimal React</h1>
+)
+
+export default MyApp
