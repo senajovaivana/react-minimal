0573a42fea1dcc70cce444511d7ce8d637f9b752
senajovaivana <senajovaivana@gmail.com>
2019-10-15 21:03:01 +0200

    Lab1Ex4

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import AppTitle from "./AppTitle";
 
+const array = [2, 56, 23, 88, 17, 4];
+
 function App() {
     return(
-        <AppTitle/>
+        <ul>
+        {array.map(item => {
+            if (item > 15) {
+                return <li>{item}</li>;
+            }
+            })}
+        </ul>
     );
 }
 
 const generateArray = (n) => Array.from({length: n}, (v, k) => k+1);
 
 const generateRandomArray = (n) => Array.from({length: n}, (v,k) => Math.floor(Math.random() * 25 ) + 1);
+
+
 
 console.log(generateRandomArray(10));
 
